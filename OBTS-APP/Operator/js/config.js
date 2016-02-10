dd7da548f764728a3d197dbe4a4a9281f6cfc2fe
alexaung <aungmo@gmail.com>
2016-02-10 18:25:26 +0800

    Latest changes

--- OBTS-APP/Operator/js/config.js
+++ OBTS-APP/Operator/js/config.js
@@ -6,13 +6,13 @@
  * Initial there are written state for all view in theme.
  *
  */
-function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdleProvider, KeepaliveProvider, $httpProvider,$locationProvider) {
+function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdleProvider, KeepaliveProvider) {
 
-    $httpProvider.interceptors.push('authInterceptorService');
     // Configure Idle settings
     IdleProvider.idle(5); // in seconds
     IdleProvider.timeout(120); // in seconds
-    $urlRouterProvider.otherwise("/dashboards/dashboard_1");
+
+    $urlRouterProvider.otherwise("/index");
 
     $ocLazyLoadProvider.config({
         // Set to true if you want to see what and when is dynamically loaded
@@ -20,300 +20,64 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdlePro
     });
 
     $stateProvider
+
         .state('login', {
             url: "/login",
             templateUrl: "views/login.html",
-            data: {pageTitle :'Login', specialClass: 'gray-bg'}
+            data: { pageTitle: 'Login', specialClass: 'gray-bg' }
         })
-        .state('dashboards', {
-            abstract: true,
-            url: "/dashboards",
-            templateUrl: "views/common/content.html",
+        .state('login_two_columns', {
+            url: "/login_two_columns",
+            templateUrl: "views/login_two_columns.html",
+            data: { pageTitle: 'Login two columns', specialClass: 'gray-bg' }
         })
-        .state('dashboards.dashboard_1', {
-            url: "/dashboard_1",
-            templateUrl: "views/dashboard_1.html",
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-
-                            serie: true,
-                            name: 'angular-flot',
-                            files: ['js/plugins/flot/jquery.flot.js', 'js/plugins/flot/jquery.flot.time.js', 'js/plugins/flot/jquery.flot.tooltip.min.js', 'js/plugins/flot/jquery.flot.spline.js', 'js/plugins/flot/jquery.flot.resize.js', 'js/plugins/flot/jquery.flot.pie.js', 'js/plugins/flot/curvedLines.js', 'js/plugins/flot/angular-flot.js', ]
-                        },
-                        {
-                            name: 'angles',
-                            files: ['js/plugins/chartJs/angles.js', 'js/plugins/chartJs/Chart.min.js']
-                        },
-                        {
-                            name: 'angular-peity',
-                            files: ['js/plugins/peity/jquery.peity.min.js', 'js/plugins/peity/angular-peity.js']
-                        },
-                        {
-                            files: ['js/plugins/footable/footable.all.min.js', 'css/plugins/footable/footable.core.css']
-                        },
-                        {
-                            name: 'ui.footable',
-                            files: ['js/plugins/footable/angular-footable.js']
-                        },
-                    ]);
-                }
-            }
+        .state('register', {
+            url: "/register",
+            templateUrl: "views/register.html",
+            data: { pageTitle: 'Register', specialClass: 'gray-bg' }
         })
-        .state('mybuses', {
-            abstract: true,
-            url: "/mybuses",
-            templateUrl: "views/common/content.html",
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            files: ['js/plugins/footable/footable.all.min.js', 'css/plugins/footable/footable.core.css']
-                        },
-                        {
-                            name: 'ui.footable',
-                            files: ['js/plugins/footable/angular-footable.js']
-                        },
-                    ]);
-                }
-            }
+        .state('lockscreen', {
+            url: "/lockscreen",
+            templateUrl: "views/lockscreen.html",
+            data: { pageTitle: 'Lockscreen', specialClass: 'gray-bg' }
         })
-        .state('mybuses.buslist', {
-            url: "/mybuses/bus_list",
-            templateUrl: "views/mybus/bus_list.html",
-            data: { pageTitle: 'Bus List' },
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            files: ['css/plugins/iCheck/custom.css', 'js/plugins/iCheck/icheck.min.js']
-                        },
-                        {
-                            name: 'datePicker',
-                            files: ['css/plugins/datapicker/angular-datapicker.css', 'js/plugins/datapicker/angular-datepicker.js']
-                        },
-                        {
-                            name: 'cgNotify',
-                            files: ['css/plugins/angular-notify/angular-notify.min.css', 'js/plugins/angular-notify/angular-notify.min.js']
-                        }                       
-                    ]);
-                }
-            }
+        .state('forgot_password', {
+            url: "/forgot_password",
+            templateUrl: "views/forgot_password.html",
+            data: { pageTitle: 'Forgot password', specialClass: 'gray-bg' }
         })
-        .state('routes', {
-            abstract: true,
-            url: "/routes",
-            templateUrl: "views/common/content.html",
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            files: ['js/plugins/footable/footable.all.min.js', 'css/plugins/footable/footable.core.css']
-                        },
-                        {
-                            name: 'ui.footable',
-                            files: ['js/plugins/footable/angular-footable.js']
-                        }
-                    ]);
-                }
-            }
+        .state('errorOne', {
+            url: "/errorOne",
+            templateUrl: "views/errorOne.html",
+            data: { pageTitle: '404', specialClass: 'gray-bg' }
         })
-        .state('routes.routelist', {
-            url: "/routes/route_list",
-            templateUrl: "views/routes/route_list.html",
-            data: { pageTitle: 'Route List' },
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                       {
-                           files: ['css/plugins/iCheck/custom.css', 'js/plugins/iCheck/icheck.min.js']
-                       },
-                        {
-                            name: 'datePicker',
-                            files: ['css/plugins/datapicker/angular-datapicker.css', 'js/plugins/datapicker/angular-datepicker.js']
-                        },
-                        {
-                            name: 'cgNotify',
-                            files: ['css/plugins/angular-notify/angular-notify.min.css', 'js/plugins/angular-notify/angular-notify.min.js']
-                        }
-                    ]);
-                }
-            }
+        .state('errorTwo', {
+            url: "/errorTwo",
+            templateUrl: "views/errorTwo.html",
+            data: { pageTitle: '500', specialClass: 'gray-bg' }
         })
-        .state('booking', {
-            abstract: true,
-            url: "/booking",
-            templateUrl: "views/common/content.html",
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            name: 'cgNotify',
-                            files: ['css/plugins/angular-notify/angular-notify.min.css', 'js/plugins/angular-notify/angular-notify.min.js']
-                        }
-                    ]);
-                }
-            }
-        })
-        .state('booking.calendar', {
-            url: "/calendar",
-            templateUrl: "views/booking/calendar.html",
-            data: { pageTitle: 'Booking Calendar' },
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            insertBefore: '#loadBefore',
-                            files: ['css/plugins/fullcalendar/fullcalendar.css', 'js/plugins/fullcalendar/fullcalendar.min.js']
-                        },
-                        {
-                            name: 'ui.calendar',
-                            files: ['js/plugins/fullcalendar/calendar.js', 'js/plugins/fullcalendar/gcal.js']
-                        }
-                    ]);
-                }
-            }
-        })
-        .state('booking.search', {
-            url: "/search",
-            templateUrl: "views/booking/search.html",
-            data: { pageTitle: 'Booking Search' },
-            resolve: {
-                        loadPlugin: function ($ocLazyLoad) {
-                            return $ocLazyLoad.load([
-                               {
-                                   files: ['css/plugins/iCheck/custom.css', 'js/plugins/iCheck/icheck.min.js']
-                               },
-                               {
-                                       
-                                        files: ['css/plugins/chosen/chosen.css','js/plugins/chosen/chosen.jquery.js']
-                               },
-                               {
-                                   name: 'datePicker',
-                                   files: ['css/plugins/datapicker/angular-datapicker.css', 'js/plugins/datapicker/angular-datepicker.js']
-                               },
-                            ]);
-                        }
-                    }
-        })
-        .state('booking.searchresult',{
-            url: "/searchresult/:FromCity/:ToCity/:DeptDate/:ReturnDate/:IsReturn",
-            templateUrl: "views/booking/searchresult.html",
-            data: { pageTitle: 'Booking Search Result' },
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                       {
-                           files: ['css/plugins/iCheck/custom.css', 'js/plugins/iCheck/icheck.min.js']
-                       },
-                        {
-                            name: 'datePicker',
-                            files: ['css/plugins/datapicker/angular-datapicker.css', 'js/plugins/datapicker/angular-datepicker.js']
-                        },
-                        {
-                            name: 'cgNotify',
-                            files: ['css/plugins/angular-notify/angular-notify.min.css', 'js/plugins/angular-notify/angular-notify.min.js']
-                        },
-                        {
-                            files: ['js/plugins/footable/footable.all.min.js', 'css/plugins/footable/footable.core.css']
-                        },
-                        {
-                            name: 'ui.footable',
-                            files: ['js/plugins/footable/angular-footable.js']
-                        },
-                    ]);
-                }
-            }
-        })
-        .state('mailbox', {
-            abstract: true,
-            url: "/mailbox",
-            templateUrl: "views/common/content.html",
-        })
-        .state('mailbox.inbox', {
-            url: "/inbox",
-            templateUrl: "views/mailbox.html",
-            data: { pageTitle: 'Mail Inbox' },
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            files: ['css/plugins/iCheck/custom.css', 'js/plugins/iCheck/icheck.min.js']
-                        }
-                    ]);
-                }
-            }
-        })
-        .state('mailbox.email_view', {
-            url: "/email_view",
-            templateUrl: "views/mail_detail.html",
-            data: { pageTitle: 'Mail detail' }
-        })
-        .state('mailbox.email_compose', {
-            url: "/email_compose",
-            templateUrl: "views/mail_compose.html",
-            data: { pageTitle: 'Mail compose' },
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            files: ['css/plugins/summernote/summernote.css', 'css/plugins/summernote/summernote-bs3.css', 'js/plugins/summernote/summernote.min.js']
-                        },
-                        {
-                            name: 'summernote',
-                            files: ['css/plugins/summernote/summernote.css', 'css/plugins/summernote/summernote-bs3.css', 'js/plugins/summernote/summernote.min.js', 'js/plugins/summernote/angular-summernote.min.js']
-                        }
-                    ]);
-                }
-            }
-        })
-        .state('mailbox.email_template', {
-            url: "/email_template",
-            templateUrl: "views/email_template.html",
-            data: { pageTitle: 'Mail compose' }
-        })
-        .state('widgets', {
-            url: "/widgets",
-            templateUrl: "views/widgets.html",
-            data: { pageTitle: 'Widhets' },
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            serie: true,
-                            name: 'angular-flot',
-                            files: ['js/plugins/flot/jquery.flot.js', 'js/plugins/flot/jquery.flot.time.js', 'js/plugins/flot/jquery.flot.tooltip.min.js', 'js/plugins/flot/jquery.flot.spline.js', 'js/plugins/flot/jquery.flot.resize.js', 'js/plugins/flot/jquery.flot.pie.js', 'js/plugins/flot/curvedLines.js', 'js/plugins/flot/angular-flot.js', ]
-                        },
-                        {
-                            files: ['css/plugins/iCheck/custom.css', 'js/plugins/iCheck/icheck.min.js']
-                        },
-                        {
-                            files: ['js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js', 'js/plugins/jvectormap/jquery-jvectormap-2.0.2.css']
-                        },
-                        {
-                            files: ['js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js']
-                        },
-                        {
-                            name: 'ui.checkbox',
-                            files: ['js/bootstrap/angular-bootstrap-checkbox.js']
-                        }
-                    ]);
-                }
-            }
-        })
+        
         .state('landing', {
             url: "/landing",
             templateUrl: "views/landing.html",
             data: { pageTitle: 'Landing page', specialClass: 'landing-page' }
-        });
-    
-    // use the HTML5 History API
-    //$locationProvider.html5Mode(true);
+        })
+
+        //.state('index', {
+        //    url: "/index",
+        //    templateUrl: "views/common/content.html",
+        //    data: { pageTitle: 'Landing page', specialClass: 'landing-page' }
+        //})
+        .state('index', {
+            url: "/index",
+            templateUrl: "views/main.html",
+            data: { pageTitle: 'Landing page', specialClass: 'landing-page' }
+        })
+        
 }
 angular
     .module('inspinia')
     .config(config)
-    .run(function ($rootScope, $state) {
+    .run(function($rootScope, $state) {
         $rootScope.$state = $state;
     });
